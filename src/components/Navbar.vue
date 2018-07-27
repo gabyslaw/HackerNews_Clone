16bb6fdd0b3bfb4cf22186d078f18a150af3c18d
gabyslaw <gabyslaw@gmail.com>
2018-07-27 13:07:32 +0100

    Initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="pure-menu pure-menu-horizontal">
+        <div class="container">
+        <a href="#" class="pure-menu-heading pure-menu-link">Home</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+.pure-menu {
+    background-color: #E17842;
+}
+.pure-menu a {
+    color: white;
+}
+</style>
